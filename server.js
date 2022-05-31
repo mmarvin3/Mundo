50ae8a8b92e0f1f8804563dcb5ab8faa7396800f
Marvin Merida <marvinmerida@Marvins-MBP.fios-router.home>
2022-05-31 15:06:57 -0400

    mongoose connection established

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
+//Dependencies
 const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const PORT = 3000;
+
+//Connection to mongoose
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+//Checking mongoose connection
+db = mongoose.connection;
+db.on("error", (err) => 
+    console.log(`${err.message} is mongodb not connected?`)
+);
+db.on("connected", () => console.log("MONGO is connected"));
+db.on("disconnected", () => console.log("mongo has disconnected"));
+
+//Middleware
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended: false}));
 
 //Listener
 app.listen(PORT, () => {
     console.log(`Listening for port ${PORT}`);
 })
-
-//Connect to mongoose
-// mongoose.connect(process.env.DATABASE_URL, {
-//     useUnifiedTopology: true,
-// })
-
-// db = mongoose.connection;
-
-// db.on("error", (err) => 
-//     console.log(`${err.message} is mongodb not connected?`)
-// )
